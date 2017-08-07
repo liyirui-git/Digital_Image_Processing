c1a90015f98c9d86631fc96a76282314b72b210a
iry-lee <1102951428@qq.com>
2017-08-07 12:41:45 +0800

    add ImageSegmentation
    新增图像分割

---
+++ project1/ImageSegmentation.java
@@ -0,0 +1,82 @@
+package project1;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class ImageSegmentation extends ImageFilter{
+	/*	File image;
+		String path; 
+		BufferedImage bi;
+		int type;
+		int m;
+		int n;
+	*/
+	int threshold;
+	
+	public ImageSegmentation(String path, int threshold) throws IOException {
+		super(path, 3, 3);           //the model is 3x3, so m=3, n=3.
+		this.threshold = threshold;
+	}
+	
+	public void Prewitt() throws IOException {
+		System.out.println("<PrewittSegmentation Start!>");
+		int x, y;
+		
+		Greyed();
+		AdaptiveFilter(7, 7, 3, 3);
+		
+		int m1 = (m-1)/2;
+		int n1 = (n-1)/2;
+		int m2,n2;
+		int [][] ImageArray = new int [bi.getWidth()][bi.getHeight()];
+		
+		for(y=0; y<bi.getHeight(); y++) {
+			for(x=0; x<bi.getWidth(); x++) {
+				int grey = 0;
+				int Gx;
+				int Gy;
+				int [] Square = new int [m*n];
+				int k = 0;
+				
+				m2 = x-m1;
+				n2 = y-n1;
+				if(m2<0) {
+					//m2 = 0;
+					continue;
+				}
+				if(n2<0) {
+					//n2 = 0;
+					continue;
+				}
+				
+				for(int i = m2; i<=x+m1 && i<bi.getWidth(); i++)
+					for(int j = n2; j<=y+n1 && j<bi.getHeight(); j++) {
+						Square[k++] = (bi.getRGB(i, j) & 0x00ffffff)>>16;
+					}
+				
+				Gx = Square[6] + Square[7] + Square[8] - (Square[0] + Square[1] + Square[2]);
+				Gy = Square[2] + Square[5] + Square[8] - (Square[0] + Square[3] + Square[6]);
+				
+				if(Math.abs(Gx)>=threshold || Math.abs(Gy)>=threshold)
+					grey = 0xff;
+				else
+					grey = 0x00;
+				
+				ImageArray[x][y] = grey*0x10101;
+			}
+			
+			
+		}
+		
+		for(x=0; x<bi.getWidth(); x++)
+			for(y=0; y<bi.getHeight(); y++)
+				bi.setRGB(x, y, ImageArray[x][y]);
+		
+		File PS = Preprocess.CreatFile("C:\\My Document\\BUAA\\Lab Project\\PrewittSegmentation.jpg");
+		ImageIO.write(bi,"jpg", PS);
+		
+		System.out.println("<PrewittSegmentation Finish!>");
+	}
+}
---
+++ project1/MainProcessing.java
@@ -0,0 +1,54 @@
+/* Data: July/15th/2017
+ * Time: 12:49 (UTC+8)
+ * Coder: Asp
+ * For What? To do some easy Image processing to help Asp understand a part of 
+ *           knowledge in Digital Image Processing......
+ *           And this part is mainly about Image Format Exchange.
+ * The Last Update Data: July/22nd/2017, 15:28(UTC+8)
+ * */
+
+
+package project1;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MainProcessing {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Preprocess.Welcome();
+		
+		String path = "C:\\My Document\\BUAA\\Lab Project\\";
+		System.out.println("Please input the file name:");
+		Scanner scanner = new Scanner(System.in);
+		String FileName =scanner.next();
+		System.out.println();
+		
+		
+		//ImageProcess IP = new ImageProcess(path+FileName);
+		//ImageFilter FT = new ImageFilter(path+FileName, 3, 3);
+		ImageSegmentation IS = new ImageSegmentation(path+FileName, 30);
+		long t1 = System.currentTimeMillis();
+		
+		//IP.PrintImage();
+		//IP.Inverse();
+		//IP.PowerTransform(0.99999);
+		//IP.LogTransform();
+		//IP.LinerGreyTransform(-40); //if n>0, ligthen; if n<0, darken.
+		//IP.GreyImage();
+		//IP.PowerGreyTransform(0.2); //if n>1, darken; if 0<n<1, ligthen.
+		//IP.HistogramEqualization();
+		
+		//FT.ArithmeticAverage();
+		//FT.StatisticalFilter(4); //if type==1, is medium statistic; if type==2, is max-statistic; if type==3, is min-statistic; if type==4, is medial-point statistic.
+		//FT.AdaptiveFilter(7, 7, 3, 3);
+		
+		IS.Prewitt();
+		
+		System.out.println("\ntotal:"+(float)(System.currentTimeMillis()-t1)/1000+"s");
+		
+		scanner.close();
+	}
+
+}
